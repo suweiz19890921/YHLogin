01ef806225082d633374e449ca33e4f697fbb5e5
苏威 <suwei@solot.com>
2017-04-17 18:51:10 +0800

    First init

---
+++ Example/Pods/Target Support Files/Pods-YHLogin_Tests/Pods-YHLogin_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YHLogin_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YHLogin_Tests
+@end
---
+++ Example/Pods/Target Support Files/YHLogin/YHLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YHLogin : NSObject
+@end
+@implementation PodsDummy_YHLogin
+@end
